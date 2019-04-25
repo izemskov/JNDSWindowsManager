342963a91074be5bf36da7caa1745214e13c9291
izemskov <pascal.ilya@gmail.com>
2019-04-25 14:43:08 +0400

    build

--- build.bat
+++ build.bat
@@ -1,8 +1,11 @@
 SET WTKDIR=c:/WTK22
 SET CP=%WTKDIR%/lib/cldcapi10.jar;%WTKDIR%/lib/midpapi20.jar;./classes.zip 
 del .\output\*.* /Q
-"c:/program files/java/jdk1.5.0_06/bin/javac.exe" -source 1.3 -target 1.1 -bootclasspath %CP% ./src/*.java ./src/ru/develgame/JNDSWindowsManager/*.java ./src/ru/develgame/JNDSWindowsManager/Components/*.java
+"c:/program files/java/jdk1.5.0_06/bin/javac.exe" -source 1.3 -target 1.1 -bootclasspath %CP% ./src/*.java ./src/ru/develgame/JNDSWindowsManager/*.java ./src/ru/develgame/JNDSWindowsManager/Forms/*.java ./src/ru/develgame/JNDSWindowsManager/Components/*.java ./src/ru/develgame/JNDSWindowsManager/Actions/*.java ./src/ru/develgame/JNDSWindowsManager/Events/*.java
 %WTKDIR%/bin/preverify -classpath %CP% ./src
+del .\src\ru\develgame\JNDSWindowsManager\Forms\*.class /Q
 del .\src\ru\develgame\JNDSWindowsManager\Components\*.class /Q
+del .\src\ru\develgame\JNDSWindowsManager\Actions\*.class /Q
+del .\src\ru\develgame\JNDSWindowsManager\Events\*.class /Q
 del .\src\ru\develgame\JNDSWindowsManager\*.class /Q
 del .\src\*.class /Q
