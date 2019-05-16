c60164a5f114f5dd81c15fd27e2aea2c85f79aa8
izemskov <pascal.ilya@gmail.com>
2019-05-16 17:14:23 +0300

    Keyboard form

--- src/ru/develgame/JNDSWindowsManager/Forms/JNDSKeyboardForm.java
+++ src/ru/develgame/JNDSWindowsManager/Forms/JNDSKeyboardForm.java
@@ -38,15 +38,75 @@ public class JNDSKeyboardForm extends JNDSComponentsForm {
         jNDSLabel = new JNDSLabel(text[0], LEFT_BORDER_X, TOP_BORDER_Y);
         addComponent(jNDSLabel);
 
-        int currentPos = LEFT_BORDER_X;
+        // 1 - 9 buttons
+        int currentPosX = LEFT_BORDER_X;
+        int currentPosY = 50;
         for (int i = 1; i <= 9; i++) {
-            JNDSButton jndsButton = new JNDSButton(Integer.toString(i), currentPos, 50);
+            JNDSButton jndsButton = new JNDSButton(Integer.toString(i), currentPosX, currentPosY);
             jndsButton.setClickAction(new KeyClickAction(this, Integer.toString(i)));
 
-            currentPos += jndsButton.getWidth() + 1;
+            currentPosX += jndsButton.getWidth() + 1;
 
             addComponent(jndsButton);
         }
+
+        // 0 button
+        JNDSButton jndsButton = new JNDSButton(Integer.toString(0), currentPosX, currentPosY);
+        jndsButton.setClickAction(new KeyClickAction(this, Integer.toString(0)));
+
+        currentPosX += jndsButton.getWidth() + 1;
+
+        addComponent(jndsButton);
+
+        currentPosY += jndsButton.getHeight() + 1;
+
+        // qwerty
+        currentPosX = LEFT_BORDER_X;
+        currentPosX += addButton("q", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("w", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("e", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("r", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("t", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("y", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("u", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("i", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("o", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("p", currentPosX, currentPosY) + 1;
+
+        currentPosY += jndsButton.getHeight() + 1;
+
+        currentPosX = LEFT_BORDER_X;
+        currentPosX += addButton("a", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("s", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("d", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("f", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("g", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("h", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("j", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("k", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("l", currentPosX, currentPosY) + 1;
+
+        currentPosY += jndsButton.getHeight() + 1;
+
+        currentPosX = LEFT_BORDER_X;
+        currentPosX += addButton("z", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("x", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("c", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("v", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("b", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("n", currentPosX, currentPosY) + 1;
+        currentPosX += addButton("m", currentPosX, currentPosY) + 1;
+    }
+
+    private int addButton(String symbol, int currentPosX, int currentPosY) {
+        JNDSButton jndsButton = new JNDSButton(symbol, currentPosX, currentPosY);
+        jndsButton.setClickAction(new KeyClickAction(this, symbol));
+
+        currentPosX += jndsButton.getWidth() + 1;
+
+        addComponent(jndsButton);
+
+        return jndsButton.getWidth();
     }
 
     public String getText() {
@@ -55,9 +115,11 @@ public class JNDSKeyboardForm extends JNDSComponentsForm {
 
     public void setText(String text) {
         if (text != null) {
-            this.text[0] = text;
-            jNDSLabel.setText(this.text[0]);
-            cursorPos = this.text[0].length() + 1;
+            if (text.length() <= MAX_TEXT_LENGTH) {
+                this.text[0] = text;
+                jNDSLabel.setText(this.text[0]);
+                cursorPos = this.text[0].length() + 1;
+            }
         }
     }
 
